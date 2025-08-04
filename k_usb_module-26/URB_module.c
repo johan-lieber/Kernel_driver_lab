86b6df448e8c1dcbab5f54bb0369066b9ad17232
Johan-liebert <i.liebert@proton.me>
2025-08-04 05:57:21 +0000

    changed  usb_sndbulkpipe to usb_rvcbulkpipe

--- k_usb_module-26/URB_module.c
+++ k_usb_module-26/URB_module.c
@@ -3,15 +3,21 @@
 #include <linux/module.h> 
 #include <linux/usb/ch9.h> 
 #include <linux/usb/storage.h> 
+#include <linux/slab.h> 
+#include <linux/types.h> 
+#include <linux/byteorder/generic.h> 
 
 
 #define  VID  (0X0781)
 #define PID  (0X5591)
 #define BUF_SIZE 512 
+#define CBW_LEN  31 
+
 
 /* GLOBAL VARIBALE */ 
 unsigned  char *bulk_buf ; 
 struct urb *my_urb ; 
+unsigned char *cbw_buffer ; 
 
 
 
@@ -19,7 +25,7 @@ struct urb *my_urb ;
 /* FUNCTION PROTOTYPE */ 
 static int usb_probe( struct usb_interface *interface ,  const struct  usb_device_id *id);
 static void usb_disconnect( struct usb_interface *interface );
-static   void my_urb_callback ( struct urb *urb ); 
+static   void cbw_callback ( struct urb *urb ); 
 
 /* USB  DEVICE ID TABLE  */ 
 const struct  usb_device_id  usb_table[] = 
@@ -40,13 +46,24 @@ MODULE_DEVICE_TABLE(usb,usb_table);
 	.id_table =  usb_table 
 }; 
 
+/*  command block wrapper structure */ 
+struct command_block_wrapper { 
+	__le32 dCBWSignature ; 
+	__le32 dCBWTag ; 
+	__le32 dCBWDataTransferLength ; 
+	__u8 bmCBWFlags ; 
+	__u8 bCBWLUN ; 
+	__u8 bCBWCBLength ; 
+	__u8 CBWCB[16];
+} __attribute__((packed)); 
 
 
 /************************************************ USB  FUNCTIONS ****************************************/
 
-static int usb_probe ( struct  usb_INTERface *interface ,  const struct usb_device_id  *id) 
-
+static int usb_probe ( struct  usb_interface *interface ,  const struct usb_device_id  *id)
 {
+	pr_info(" USB PROBE \n"); 
+
 
 	struct usb_device *dev  =  interface_to_usbdev(interface) ; 
 	struct usb_host_interface  *iface_desc = interface->cur_altsetting ; 
@@ -57,7 +74,7 @@ static int usb_probe ( struct  usb_INTERface *interface ,  const struct usb_devi
 	
 	
 
-	for(int i = 0 ; i < iface_desc->desc.bNumEndpoints ; ++i ) 
+	for(int i = 0 ; i < iface_desc->desc.bNumEndpoints ; i++ ) 
 	{ 
 		struct usb_endpoint_descriptor  *epd = &iface_desc->endpoint[i].desc; 
 		
@@ -87,6 +104,16 @@ static int usb_probe ( struct  usb_INTERface *interface ,  const struct usb_devi
 
 
 
+	 /*Allocating buffer  for CW_LEN */ 
+	 cbw_buffer = kmalloc(CBW_LEN , GFP_KERNEL) ; 
+	 if( cbw_buffer == NULL ) 
+	 { 
+		 pr_info(" CW_BUF_ALLOC_ERR\n"); 
+		 return ENOMEM ; 
+	 } 
+
+
+
 	 /* Allocating and  urb */ 
 	 my_urb = usb_alloc_urb(0, GFP_KERNEL) ; 
 	 
@@ -98,7 +125,7 @@ static int usb_probe ( struct  usb_INTERface *interface ,  const struct usb_devi
 
 
 
-	 usb_fill_bulk_urb(my_urb, dev , usb_sndbulkpipe(dev, bulk_out_endpointaddr), bulk_buf , BUF_SIZE , my_urb_callback , NULL ) ; 
+	 usb_fill_bulk_urb(my_urb, dev , usb_rcvbulkpipe(dev, bulk_out_endpointaddr), cbw_buffer , CBW_LEN , cbw_callback , NULL ) ; 
 
 
 	 int ret = usb_submit_urb(my_urb , GFP_KERNEL); 
@@ -142,19 +169,56 @@ static void usb_disconnect ( struct usb_interface  *interface )
 
 
 
-/* my_urb_callback function */ 
+/* cbw_callback function */ 
 
-static   void my_urb_callback ( struct urb *urb )
+static   void cbw_callback ( struct urb *urb )
 { 
+	pr_info(" Callback usb functions \n"); 
+
+
 	if(urb->status)
 	{
-		pr_err(" URB completed  with error : %d\n", urb->status) ; 
-	}else{ 
-		pr_info("URB  success , recieved %d  bytes \n" , urb->actual_length); 
+		pr_err("  CBW URB  FAILED WIH STATUS  : %d\n", urb->status) ; 
+		return ; 
+	} 
+
+	struct command_block_wrapper *cbw =  (struct  command_block_wrapper *)  urb->transfer_buffer; 
+
+	if( cbw->dCBWSignature !=cpu_to_le32(0x43425355)) 
+	{ 
+		pr_err(" Imvalid CBW signature \n"); 
+		return ; 
+	} 
+
+//	pr_info("Received  CBW - Command : 0x%02\n", cbw->CBWCB[0]); 
+
+	switch(cbw->CBWCB[0]) 
+	{ 
+		case 0x12 : 
+			pr_info(" SCSI  INQUIRY command  Recieved \n") ;
+			break ; 
+		case 0x00:
+			pr_info(" SCSI TEST UNIT READY \n"); 
+			break ; 
+		case 0x28 : 
+			pr_info("SCSI  READ (10) \n") ;
+			break ;
+		case 0x1A : 
+			pr_info(" SCSI MODE SENSE COMMAND RECIEVED \n"); 
+			break ; 
+		case 0x2A : 
+			pr_info(" SCSI WRITE COmmand RECIEVED \n"); 
+			break ; 
+
+		default :
+			pr_warn(" Unhandled SCsi command :0x%02x\n", cbw->CBWCB[0]); 
+			break ; 
 	}
 
 	return ; 
+	
 } 
+ 
 
 
 
